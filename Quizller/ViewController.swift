dcd139202576eb1f342f354ac3ca8f226bc51b34
EgorK2606 <kramere26@gmail.com>
2020-07-31 10:30:59 +0200

    First commit. UI little bit had started

---
+++ Quizller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quizller
+//
+//  Created by Egor  on 31.07.2020.
+//  Copyright © 2020 Gregor Kramer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
