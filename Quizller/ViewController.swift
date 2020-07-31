69938d0a82a3d215cf5b63f5746121f8666c622e
EgorK2606 <kramere26@gmail.com>
2020-07-31 11:00:26 +0200

    Second commit on Develop. App before implimentation of MVC

--- Quizller/ViewController.swift
+++ Quizller/ViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    
+    let quiz = [
+    ["Aaaaaaaa", "True"],
+    ["Bbbbbb", "True"],
+    ["Cccc","False"]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
 
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            sender.backgroundColor = .systemGreen
+        } else {
+            sender.backgroundColor = .systemRed
+        }
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    
+    }
+    
+    
+    @objc func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+        trueButton.backgroundColor = .clear
+        falseButton.backgroundColor = .clear
+        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
 
+    }
+    
 }
 
